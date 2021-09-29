fea77411eef438172ab471d66e2fd11c7abfde98
zyu9 <zyu9@uncc.edu>
2021-09-29 18:03:41 -0400

    update

--- itis3135/scripts/polygon.js
+++ itis3135/scripts/polygon.js
@@ -1,34 +1,67 @@
 //Show mascot
 var shapeSides = prompt("Please enter a variable for Zealous Yoranian: ");
-while(isNaN(shapeSides) || shapeSides > 10){
+while(validEntry(shapeSides)){
     shapeSides = prompt("Please enter a valid number: "); 
 }
 
-var shapeName = ""; 
+var nameOfShape = ""; 
+var numberOfSides = 0; 
 
 if(shapeSides%1 === 0){
     //The entry is integer
-    var intEntry = parseInt(shapeSides); 
+    numberOfSides = parseInt(shapeSides); 
 
     //If negative, use absolute value
-    if(intEntry < 0){
-        intEntry = Math.abs(intEntry); 
+    if(numberOfSides < 0){
+        numberOfSides = Math.abs(numberOfSides); 
     }
-    alert("intEntry" + intEntry); 
+    //alert("intEntry" + intEntry); 
 }else{
     //The entry is decimal
-    var decEntry = parseFloat(shapeSides); 
+    numberOfSides = parseFloat(shapeSides); 
 
     //If negative, use absolute value
-    if(shapeSides < 0){
-        decEntry = Math.abs(decEntry); 
+    if(numberOfSides < 0){
+        numberOfSides = Math.abs(numberOfSides); 
     }
     
     //Round decimal entry to nearest integer
-    decEntry = Math.round(decEntry); 
-    alert("decEntry" + decEntry); 
+    numberOfSides = Math.round(numberOfSides); 
+    //alert("decEntry" + decEntry); 
 }
 
+//Get results
+nameOfShape = getShape(numberOfSides); 
+alert("Your shape's number of sides:   " + numberOfSides + "  Shapes:  " + nameOfShape); 
+
+function validEntry(entry){
+    var valid = false; 
+    if(isNaN(entry) || entry > 10){
+        valid = true; 
+    }
+    return valid; 
+}
+
+function getShape(sides){
+    var shapeName = ""; 
+    switch (sides){
+        case 0: shapeName = "No shape"; break; 
+        case 1: shapeName = "Monogon"; break; 
+        case 2: shapeName = "Bigon"; break; 
+        case 3: shapeName = "Trigon"; break; 
+        case 4: shapeName = "Tetragon"; break; 
+        case 5: shapeName = "Pentagon"; break; 
+        case 6: shapeName = "Hexagon"; break; 
+        case 7: shapeName = "Septagon"; break; 
+        case 8: shapeName = "Octagon"; break; 
+        case 9: shapeName = "Nonagon"; break; 
+        case 10: shapeName = "Decagon"; break; 
+    }
+
+    return shapeName; 
+}
+
+/**
 if(intEntry == 0 || decEntry == 0){
     shapeName = "No shape";
 }else if(intEntry == 1 || decEntry == 1){
@@ -52,9 +85,6 @@ if(intEntry == 0 || decEntry == 0){
 }else if(intEntry == 10 || decEntry == 10){
     shapeName = "Decagon";
 }
-//alert("shapeName in get shape" + shapeName); 
-alert("Shapessides: " + shapeSides + "Shapes: " + shapeName); 
-
 
 //var result = validEntry(shapeSides); 
 //var valid = result.valid, s = result.s;
@@ -66,7 +96,7 @@ alert("Shapessides: " + shapeSides + "Shapes: " + shapeName);
 //var shape = getShape(shapeSides); 
 //alert("Your shape is: " + shape + " with " + s + " number of sides");
 
-/**
+
 //function getShape(sides){
     var shapeName = ""; 
     switch (sides){
@@ -86,7 +116,7 @@ alert("Shapessides: " + shapeSides + "Shapes: " + shapeName);
     return shapeName; 
 //}
 
-//function validEntry(entry){
+function validEntry(entry){
     var valid = true; 
     //Get absolute value for the entry number
     var s = Math.abs(entry); 
