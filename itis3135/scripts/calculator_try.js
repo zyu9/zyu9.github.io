285f82638186be8fffa22905a2a688abcef216f7
zyu9 <zyu9@uncc.edu>
2021-09-30 17:06:42 -0400

    try calculator

---
+++ itis3135/scripts/calculator_try.js
@@ -0,0 +1,45 @@
+function buttonAddition(x, y){
+    var result = x + y; 
+    alert("The sum is: " + result); 
+    //return result; 
+}
+
+function buttonSubtraction(x, y){
+    var result = x - y; 
+    alert("The result is: " + result); 
+    //return result; 
+}
+
+function buttonMultiplication(x, y){
+    var result = x * y; 
+    alert("The result is: " + result); 
+    //return result; 
+}
+
+function buttonDivision(x, y){
+    var result = x / y; 
+    alert("The result is: " + result); 
+    //return result; 
+}
+
+function buttonModulus(x, y){
+    var result = x % y; 
+    alert("The result is: " + result); 
+    //return result; 
+}
+
+function buttonPower(x, y){
+    var result = Math.pow(x,y); 
+    alert("The result is: " + result); 
+    //return result; 
+}
+
+var number1 = prompt("Enter first number: "); 
+var number2 = prompt("\nEnter second number: "); 
+
+document.getElementById("btnAdd").addEventListener("click", buttonAddition(number1, number2));
+document.getElementById("btnSub").addEventListener("click", buttonSubtraction(number1, number2));
+document.getElementById("btnMul").addEventListener("click", buttonMultiplication(number1, number2)); 
+document.getElementById("btnDivid").addEventListener("click", buttonDivision(number1, number2)); 
+document.getElementById("btnMod").addEventListener("click", buttonModulus(number1, number2));
+document.getElementById("btnPow").addEventListener("click", buttonPower(number1, number2)); 
