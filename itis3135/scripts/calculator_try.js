c2f2202f6e1d03cd4d609b6937779d9d0eb03697
zyu9 <zyu9@uncc.edu>
2021-10-02 12:45:31 -0400

    Update calculator_try.js

--- itis3135/scripts/calculator_try.js
+++ itis3135/scripts/calculator_try.js
@@ -1,49 +1,31 @@
-function buttonAddition(x, y){
-    var result = parseInt(x) + parseInt(y); 
-    alert("The sum is: " + result); 
-    //return result; 
-}
-
-function buttonSubtraction(x, y){
-    var result = parseInt(x) - parseInt(y); 
-    alert("The result is: " + result); 
-    //return result; 
-}
-
-function buttonMultiplication(x, y){
-    var result = parseInt(x) * parseInt(y); ; 
-    alert("The result is: " + result); 
-    //return result; 
-}
-
-function buttonDivision(x, y){
-    var result = parseInt(x) / parseInt(y); 
-    alert("The result is: " + result); 
-    //return result; 
-}
-
-function buttonModulus(x, y){
-    var result = parseInt(x) % parseInt(y); 
-    alert("The result is: " + result); 
-    //return result; 
-}
-
-function buttonPower(x, y){
-    var result = Math.pow(x,y); 
-    alert("The result is: " + result); 
-    //return result; 
-}
-
 var number1 = prompt("Enter first number: "); 
 var number2 = prompt("\nEnter second number: "); 
 
 document.getElementById("btnAdd").addEventListener("click", function(number1, number2){
-    var result = parseInt(number1) + parseInt(number2); 
+    var result = parseFloat(number1) + parseFloat(number2); 
     alert("The sum is: " + result); 
 });
 
-document.getElementById("btnSub").addEventListener("click", buttonSubtraction(number1, number2));
-document.getElementById("btnMul").addEventListener("click", buttonMultiplication(number1, number2)); 
-document.getElementById("btnDivid").addEventListener("click", buttonDivision(number1, number2)); 
-document.getElementById("btnMod").addEventListener("click", buttonModulus(number1, number2));
-document.getElementById("btnPow").addEventListener("click", buttonPower(number1, number2)); 
+document.getElementById("btnSub").addEventListener("click", function(number1, number2){
+    var result = parseFloat(number1) - parseFloat(number2); 
+    alert("The result is: " + result); 
+});
+
+document.getElementById("btnMul").addEventListener("click", function(number1, number2){
+    var result = parseFloat(number1) * parseFloat(number2); 
+    alert("The result is: " + result); 
+});
+document.getElementById("btnDivid").addEventListener("click", function(number1, number2){
+    var result = parseFloat(number1) / parseFloat(number2); 
+    alert("The result is: " + result); 
+});
+
+document.getElementById("btnMod").addEventListener("click", function(number1, number2){
+    var result = parseFloat(number1) % parseFloat(number2); 
+    alert("The result is: " + result); 
+});
+
+document.getElementById("btnPow").addEventListener("click", function(number1, number2){
+    var result = Math.pow(parseFloat(number1), parseFloat(number2)); 
+    alert("The result is: " + result); 
+});
