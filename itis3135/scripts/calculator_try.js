52674c8bf2bae6f38e4b5fe69bb8366468085201
zyu9 <zyu9@uncc.edu>
2021-10-02 12:38:48 -0400

    Update calculator_try.js

--- itis3135/scripts/calculator_try.js
+++ itis3135/scripts/calculator_try.js
@@ -37,7 +37,10 @@ function buttonPower(x, y){
 var number1 = prompt("Enter first number: "); 
 var number2 = prompt("\nEnter second number: "); 
 
-document.getElementById("btnAdd").addEventListener("click", buttonAddition(number1, number2));
+document.getElementById("btnAdd").addEventListener("click", function(number1, number2){
+    var result = parseInt(x) + parseInt(y); 
+    alert("The sum is: " + result); 
+});
 document.getElementById("btnSub").addEventListener("click", buttonSubtraction(number1, number2));
 document.getElementById("btnMul").addEventListener("click", buttonMultiplication(number1, number2)); 
 document.getElementById("btnDivid").addEventListener("click", buttonDivision(number1, number2)); 
