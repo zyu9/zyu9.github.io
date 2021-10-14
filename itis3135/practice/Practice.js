43fdd8f7ca90aa3b2d86b37d9abf757f019423b7
zyu9 <zyu9@uncc.edu>
2021-10-14 16:57:22 -0400

    updated

--- itis3135/practice/Practice.js
+++ itis3135/practice/Practice.js
@@ -2,13 +2,16 @@ var $ = function(id) {
 	return document.getElementById(id);
 };
 
+function click(){
+	alert("image clicked!");
+}
+
 window.onload = function() {
 
 
 	//add onclick event handler for each image
-
 	// for click event add item to order and update total
-
+	$("menu").onclick = click; 
 	// display order and total
 
 	
