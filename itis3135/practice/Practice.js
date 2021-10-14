135852400e12ed8f0b2668357b1d2bef83bb3ca6
zyu9 <zyu9@uncc.edu>
2021-10-14 17:27:24 -0400

    updated

--- itis3135/practice/Practice.js
+++ itis3135/practice/Practice.js
@@ -34,6 +34,7 @@ function cappuccino(){
 function calculateTotal(){
 	if(n > 0 || m > 0){
 		total = parseFloat((1.95 * n + 3.45 * m).toFixed(2));
+		
 		var dTotal = document.createElement("p");
 		dTotal.textContent = "Total: " + total; 
 		var displayTotal = $("total");
