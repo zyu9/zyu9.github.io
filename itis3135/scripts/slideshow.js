4f53c4849f3fd1d2246c6b53362d55d07a793005
zyu9 <zyu9@uncc.edu>
2021-10-22 19:23:45 -0400

    updated

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -22,16 +22,3 @@
     // move focus to first thumbnail 
     $("li:first-child a").focus(); 
 }); // end ready  */
-
-var imgDuration = 5000;
-var fadeSpeed = 2000;
-var container = $('#slideshow-container');
-var curIndex = -1;
-
-function slideShow() {
- container + $('img.slideshow_images').eq(curIndex).fadeOut(fadeSpeed);
- container + $('img.slideshow_images').eq(curIndex  = curIndex < container.children().length - 1 ? curIndex + 1 : 0).fadeIn(fadeSpeed);
- setTimeout("slideShow()", imgDuration);
-}
-
-slideShow();
