f5362df02807a2883de2a6eb2561e011dc8433ff
zyu9 <zyu9@uncc.edu>
2021-10-23 09:58:25 -0400

    updated

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -1,4 +1,4 @@
-var slideIndex = 1;
+/* var slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
@@ -27,4 +27,29 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
-}
+} */
+
+$(document).ready(function() { 
+    // preload images 
+    $("#image_list a").each(function() { 
+        var swappedImage = new Image(); 
+        swappedImage.src = $(this).attr("href"); 
+    }); 
+ 
+    // set up event handlers for links     
+    $("#image_list a").click(function(evt) { 
+        // swap image 
+        var imageURL = $(this).attr("href"); 
+        $("#main_image").attr("src", imageURL); 
+         
+        //swap caption 
+        var caption = $(this).attr("title"); 
+        $("#caption").text(caption); 
+                 
+        // cancel the default action of the link 
+        evt.preventDefault();  // jQuery cross-browser method 
+    }); // end click 
+ 
+    // move focus to first thumbnail 
+    $("li:first-child a").focus(); 
+}); // end ready 
