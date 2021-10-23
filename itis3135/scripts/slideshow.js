a87a53a48d1e11c3adb32a0bc1f36b87fb1a71dc
zyu9 <zyu9@uncc.edu>
2021-10-23 11:55:30 -0400

    updated

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -1,35 +1,7 @@
-/* var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slideshow_slides");
-  var dots = document.getElementsByClassName("slideshow_thumbnail");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-} */
-
 $(document).ready(function() { 
+    var images = []; 
+    var titles = []; 
+
     // preload images 
     $("#image_list a").each(function() { 
         var swappedImage = new Image(); 
@@ -52,4 +24,22 @@ $(document).ready(function() {
  
     // move focus to first thumbnail 
     $("li:first-child a").focus(); 
+
+    $(".prev").click(function(evt){
+        $("#image_list a").each(function(i) { 
+            images.push($(this).attr("href"));
+            titles.push($(this).attr("title")); 
+
+            // swap image 
+            var imageURL = images[i-1]; 
+            $("#main_image").attr("src", imageURL); 
+            
+            //swap caption 
+            var caption = titles[i-1]; 
+            $("#caption").text(caption); 
+        }); 
+        
+        // cancel the default action of the link 
+        evt.preventDefault();  // jQuery cross-browser method 
+    }); 
 }); // end ready 
