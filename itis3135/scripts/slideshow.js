b8f01ec2b5e04282d52be091149b2654a0f5beef
zyu9 <zyu9@uncc.edu>
2021-10-24 11:06:13 -0400

    Update slideshow.js

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -1,18 +1,21 @@
 $(document).ready(function() { 
+    var index = 0; 
     var images = []; 
     var titles = []; 
-
+ 
     // preload images 
     $("#image_list a").each(function() { 
         images.push($(this).attr("href"));
-        titles.push($(this).attr("title")); 
+        titles.push($(this).attr("href")); 
 
         var swappedImage = new Image(); 
         swappedImage.src = $(this).attr("href"); 
     }); 
- 
+
     // set up event handlers for links     
     $("#image_list a").click(function(evt) { 
+        index = $("#image_list a").index(this); 
+
         // swap image 
         var imageURL = $(this).attr("href"); 
         $("#main_image").attr("src", imageURL); 
@@ -29,26 +32,37 @@ $(document).ready(function() {
     $("li:first-child a").focus(); 
 
     $(".prev").click(function(){
-        $("#image_list a").attr("href", function(i) { 
-            // swap image 
-            var imageURL = images[i-1]; 
-            $("#main_image").attr("src", imageURL); 
-            
-            //swap caption 
-            var caption = titles[i-1]; 
-            $("#caption").text(caption); 
-        }); 
+        if(index  < images.length - 1 || index > 0)
+        {
+            index = index - 1;
+        }
+
+        // swap image 
+        var imageURL = images[index]; 
+        $("#main_image").attr("src", imageURL); 
+        
+        //swap caption 
+        var caption = titles[index]; 
+        $("#caption").text(caption); 
+
+        // cancel the default action of the link 
+        evt.preventDefault();  // jQuery cross-browser method 
     }); 
 
     $(".next").click(function(){
-        $("#image_list a").attr("href", function(i)  { 
-            // swap image 
-            var imageURL = images[i+1]; 
-            $("#main_image").attr("src", imageURL); 
-            
-            //swap caption 
-            var caption = titles[i+1]; 
-            $("#caption").text(caption); 
-        }); 
+        if(index  < images.length - 1 || index > 0)
+        {
+            index = index + 1;
+        }
+
+        // swap image 
+        var imageURL = images[index]; 
+        $("#main_image").attr("src", imageURL); 
+        
+        //swap caption 
+        var caption = titles[index]; 
+        $("#caption").text(caption); 
+        // cancel the default action of the link 
+        evt.preventDefault();  // jQuery cross-browser method 
     }); 
 }); // end ready 
