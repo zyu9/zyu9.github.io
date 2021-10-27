b7393d9842fe504df680f21e3c77e729c913dbe4
zyu9 <zyu9@uncc.edu>
2021-10-27 14:12:09 -0400

    Update Midterm.js

--- itis3135/midterm/Midterm.js
+++ itis3135/midterm/Midterm.js
@@ -3,7 +3,21 @@ var calories = [3000,2500,1500,4000,2200,1200,4400];
 
 var $ = function(id) { return document.getElementById(id); };
 
+function updateCalorie(){
+    var day = $('choose_day').value; 
+    var calory = $('input_calorie').value; 
+    alert(day + calory);
+}
+
+function showAverageCalories(){
+
+}
+
+function  showMax(){
+
+}
+
 window.onload = function() {
     //event handlers
-
+    $('update').onclick = updateCalorie; 
 };
