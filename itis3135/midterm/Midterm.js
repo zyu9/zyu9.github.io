b57e4a0033a340d1a98c65f4def2afaf582c0707
zyu9 <zyu9@uncc.edu>
2021-10-27 13:34:24 -0400

    updated

---
+++ itis3135/midterm/Midterm.js
@@ -0,0 +1,9 @@
+var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+var calories = [3000,2500,1500,4000,2200,1200,4400];
+
+var $ = function(id) { return document.getElementById(id); };
+
+window.onload = function() {
+    //event handlers
+
+};
