5847f0fefe47d0e34582eeb28efca7656f30701a
zyu9 <zyu9@uncc.edu>
2021-11-06 11:33:00 -0400

    Update Yu-Activity10.js

--- itis3135/activityten/Yu-Activity10.js
+++ itis3135/activityten/Yu-Activity10.js
@@ -6,15 +6,16 @@ $(document).ready(function() {
         });
         // set up the event handlers for each link
         $("#image_list a").click(function(evt) { 
+                // get the image URL and caption for each image and animate the caption
+
                 // swap image 
                 var imageURL = $(this).attr("href"); 
-                $("#main_image").attr("src", imageURL); 
+                //$(this).fadeOut(3000);
+                $("#main_image").fadeOut(3000).attr("src", imageURL); 
                  
                 //swap caption 
                 var caption = $(this).attr("title"); 
                 $("#caption").text(caption); 
-
-                // get the image URL and caption for each image and animate the caption
                          
                 // cancel the default action of the link 
                 evt.preventDefault();  // jQuery cross-browser method 
