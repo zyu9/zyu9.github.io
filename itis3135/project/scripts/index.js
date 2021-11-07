0a247f767d1178c5279fd2ddd866590a0d7a1f43
zyu9 <zyu9@uncc.edu>
2021-11-07 12:30:31 -0500

    Update index.js

--- itis3135/project/scripts/index.js
+++ itis3135/project/scripts/index.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     var nextSlide = $("#slides img:first-child");
     var nextSlideSource; 
 
-    setInterval(function(){
+    var runSlideShow = function(){
         $("#slide").fadeOut(1000, function(){
             if (nextSlide.next().length == 0){
                 nextSlide = $("#slides img:first-child");
@@ -12,5 +12,16 @@ $(document).ready(function(){
                 $("#slide").attr("src", nextSlideSource).fadeIn(1000); 
             }
         });
-    }, 3000);
+    }; 
+
+    var timer1 = setInterval(runSlideShow, 3000);
+
+    $("#slide").click(function(){
+        if(timer1 != null){
+            clearInterval(timer1); 
+            timer1 = null; 
+        }else{
+            timer1 = setInterval(runSlideShow, 3000);
+        }
+    });
 });
