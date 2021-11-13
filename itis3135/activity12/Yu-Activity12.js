f50938f987ac897e078df6404f1592fe3ed69c5b
zyu9 <zyu9@uncc.edu>
2021-11-13 16:55:31 -0500

    updated activity12

--- itis3135/activity12/Yu-Activity12.js
+++ itis3135/activity12/Yu-Activity12.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
     $("#slider").bxSlider({
         auto: true,
-        minSlides: 2,
-        maxSlides: 2,
-        slideWidth: 300,
-        slideMargin: 20
+        randomStart: true, 
+        captions: true, 
+        speed: 3000, 
+        minSlides: 1,
+        maxSlides: 1,
+        moveSlides: 1, 
+        slideWidth: 500,
+        slideMargin: 20,
+        pager: true, 
+        pagerSelector: "#id_pager"
     });
 });
