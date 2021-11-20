f3766965f2f35c55e8b42224f8fdfdd9e692594c
zyu9 <zyu9@uncc.edu>
2021-11-20 13:38:43 -0500

    Update Yu-Activity14.js

--- itis3135/activity14/Yu-Activity14.js
+++ itis3135/activity14/Yu-Activity14.js
@@ -2,17 +2,18 @@ $(document).ready(function() {
     $("#nav_list a").click(function(){
         var url = $(this).attr("title") + ".json"; 
         $.getJSON(url, function(data){ 
-            alert(url);
+            alert("a");
             $.each(data, function(){ 
                 $.each(this, function(key, value){ 
+                    alert("b");
                     $("#content h1").append(value.title); 
                     $("#content h2").append(value.month);
                     $("#content h3").append(value.speaker);
                     $("#content img").attr("src", value.image);
                     $("#content p").append(value.text);
-                })
-            })
-        })
-    })
+                });
+            });
+        });
+    });
 	
 }); // end ready
