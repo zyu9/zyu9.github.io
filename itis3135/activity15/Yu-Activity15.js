eb5b98a5f5d07283e36e92ed1553e702ca18b395
zyu9 <zyu9@uncc.edu>
2021-11-22 12:44:26 -0500

    updated activity15

--- itis3135/activity15/Yu-Activity15.js
+++ itis3135/activity15/Yu-Activity15.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-
+    $.getJSON("facultyList.json", function(data){ 
+        $.each(data, function() { 
+            $.each(this, function(key, value) { 
+                $("#faculty").append( 
+                    "<img>" + value.image + "</>" +  
+                    "<h2>" + value.full_name + "</h2>" + 
+                    "<h3>" + value.department + "</h3>" +
+                    "<p>" + value.bio + "</p" 
+                ); 
+            }); 
+        });  
+    }); 
 });
