ba31f38797e536f0cfe6e7b98d9b018ef268ba14
zyu9 <zyu9@uncc.edu>
2021-11-22 14:31:42 -0500

    updated activity16

--- itis3135/activity16/Yu-Activity16.js
+++ itis3135/activity16/Yu-Activity16.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
+    var url = 
+    "http://api.flickr.com/services/feeds/photos_public.gne?" + 
+    "id=82407828@N07&format=json&jsoncallback=?&tags=vectacorpbuilding";
 
+    $.getJSON(url, function(data) { 
+        var html = ""; 
+        $.each(data.items, function(i, item){ 
+            html += "<a href=" +  item.media.m + "data-lightbox=" + "building" + "data-title=" + item.title + ">" + "<img src=" + item.media.m + ">" +"</a>"; 
+        }); 
+        $("#new_building").html(html); 
+    }); 
+
+    lightbox.option({
+        'resizeDuration': 200,
+        'wrapAround': true
+    });
 });
