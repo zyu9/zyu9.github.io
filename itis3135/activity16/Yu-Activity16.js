a2f60befb9012cf779d638e5c980420a9495d988
zyu9 <zyu9@uncc.edu>
2021-11-22 14:42:44 -0500

    Update Yu-Activity16.js

--- itis3135/activity16/Yu-Activity16.js
+++ itis3135/activity16/Yu-Activity16.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     var url = 
-    "http://api.flickr.com/services/feeds/photos_public.gne?" + 
+    "https://api.flickr.com/services/feeds/photos_public.gne?" + 
     "id=82407828@N07&format=json&jsoncallback=?&tags=vectacorpbuilding";
 
     $.getJSON(url, function(data) { 
