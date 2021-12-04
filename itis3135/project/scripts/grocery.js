7fda4950d1fcd7298cc0633f4b48b56b6793f317
zyu9 <zyu9@uncc.edu>
2021-12-04 17:06:34 -0500

    updated project

--- itis3135/project/scripts/grocery.js
+++ itis3135/project/scripts/grocery.js
@@ -1,14 +1,37 @@
-$(document).ready(function () {
-    $('#cart').simpleCart();
-});
+$(function () {
 
-$(document).ready(function () {
-    $('#cart').simpleCart({
-      addtoCartClass: '.sc-add-to-cart',
-      cartProductListClass: '.cart-products-list',
-      totalCartCountClass: '.total-cart-count',
-      totalCartCostClass: '.total-cart-cost',
-      showcartID : '#show-cart',
-      itemCountClass : '.item-count'
+    var goToCartIcon = function($addTocartBtn){
+      var $cartIcon = $(".my-cart-icon");
+      var $image = $('<img width="30px" height="30px" src="' + $addTocartBtn.data("image") + '"/>').css({"position": "fixed", "z-index": "999"});
+      $addTocartBtn.prepend($image);
+      var position = $cartIcon.position();
+      $image.animate({
+        top: position.top,
+        left: position.left
+      }, 500 , "linear", function() {
+        $image.remove();
+      });
+    }
+  
+    $('.my-cart-btn').myCart({
+      classCartIcon: 'my-cart-icon',
+      classCartBadge: 'my-cart-badge',
+      affixCartIcon: true,
+      checkoutCart: function(products) {
+        $.each(products, function(){
+          console.log(this);
+        });
+      },
+      clickOnAddToCart: function($addTocart){
+        goToCartIcon($addTocart);
+      },
+      getDiscountPrice: function(products) {
+        var total = 0;
+        $.each(products, function(){
+          total += this.quantity * this.price;
+        });
+        return total * 0.5;
+      }
     });
-});
+  
+  });
