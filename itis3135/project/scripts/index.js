14beafc084e0330562addb34e4f3998b26178494
zyu9 <zyu9@uncc.edu>
2021-12-04 20:37:14 -0500

    added slider

--- itis3135/project/scripts/index.js
+++ itis3135/project/scripts/index.js
@@ -1,1 +1,12 @@
-$('#slideshow-2').slideshow();
+$(document).ready(function() {
+    $("#slider").bxSlider({
+        auto: true,
+        randomStart: true, 
+        speed: 3000, 
+        minSlides: 1,
+        maxSlides: 1,
+        moveSlides: 1, 
+        slideWidth: 500,
+        slideMargin: 20
+    });
+});
