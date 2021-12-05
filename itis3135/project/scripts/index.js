cc42c57eceae1c8abc254d6a5cf0f23c10c471cb
zyu9 <zyu9@uncc.edu>
2021-12-05 15:55:42 -0500

    updated

--- itis3135/project/scripts/index.js
+++ itis3135/project/scripts/index.js
@@ -9,4 +9,15 @@ $(document).ready(function() {
         slideWidth: 500,
         slideMargin: 20
     });
+
+    $("#slider_featured").bxSlider({
+        auto: true,
+        randomStart: true, 
+        speed: 3000, 
+        minSlides: 1,
+        maxSlides: 1,
+        moveSlides: 1, 
+        slideWidth: 500,
+        slideMargin: 20
+    });
 });
