b687ba9e4e901f4047cfe9ebfc367760fa0db2af
zyu9 <zyu9@uncc.edu>
2021-12-05 16:33:50 -0500

    updated faq page

---
+++ itis3135/project/scripts/faqs.js
@@ -0,0 +1,8 @@
+//<!-- jQuery call to the accordion() method. -->
+$(document).ready(function() {
+    $("#accordion").accordion({
+        event: "click",
+        heightStyle: "content",
+        collapsible: true
+    });
+});
