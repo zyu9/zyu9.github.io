1c83b89d3f117d765d428f679d623039ba890c30
zyu9 <zyu9@uncc.edu>
2021-12-07 16:55:48 -0500

    updated

--- itis3135/project/scripts/shop.js
+++ itis3135/project/scripts/shop.js
@@ -15,12 +15,13 @@ $(document).ready(function() {
             $.each(data, function() { 
                 $.each(this, function(key, value) { 
                     $("#flowers").append( 
+                        "<div class=" + "col-md-3 text-center" + "data-tages=" + "flowers" + ">" +
                         "<img src= "+ value.image + ">" +  
-                        "<br>" + value.name +  "<strong>" + "$" +
+                        "<br>" + value.name + " - " + "<strong>" + "$" +
                         value.price + "<strong>" + "<br>" + 
                         "<button class=" + "btn btn-danger my-cart-btn" + "data-id=" + value.id + "data-name=" + value.name + 
                         "data-summary=" + value.name +  "data-price=" + value.price + "data-quantity=" + "1" + "data-image=" + value.image
-                        + ">" + "Add to Cart"+ "</button>"
+                        + ">" + "Add to Cart"+ "</button>" + "</div>"
                     ); 
                 }); 
             });  
