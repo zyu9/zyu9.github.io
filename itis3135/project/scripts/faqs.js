eb902b57924f4d6c63e161b88ea73af439510eda
zyu9 <zyu9@uncc.edu>
2021-12-07 17:10:35 -0500

    updated faqs json

--- itis3135/project/scripts/faqs.js
+++ itis3135/project/scripts/faqs.js
@@ -1,5 +1,29 @@
 //<!-- jQuery call to the accordion() method. -->
 $(document).ready(function() {
+    $.ajax({
+        type: "get",
+        url: "faqs.json",
+        beforeSend: function() {
+            $("#text").html("Loading...");
+        },
+        timeout: 10000,
+        error: function(json, status, error) {
+            alert("Error: " + json.status + " - " + error);
+        },
+        dataType: "json",
+        success: function(data) {
+            $("#text").html("");
+            $.each(data, function() { 
+                $.each(this, function(key, value) { 
+                    $("#text").append( 
+                        "<h3>" + value.question + "</h3>" +
+                        "<p>" + value.answer + "</p>"
+                    ); 
+                }); 
+            });  
+        }
+    });
+
     $("#accordion").accordion({
         event: "click",
         heightStyle: "content",
