c6c9ee305b1fa830dfa5c43c90f237d3912a99e4
zyu9 <zyu9@uncc.edu>
2021-12-07 16:50:48 -0500

    updated json to shopping

---
+++ itis3135/project/scripts/shop.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $.ajax({
+        type: "get",
+        url: "shop.json",
+        beforeSend: function() {
+            $("#flowers").html("Loading...");
+        },
+        timeout: 10000,
+        error: function(json, status, error) {
+            alert("Error: " + json.status + " - " + error);
+        },
+        dataType: "json",
+        success: function(data) {
+            $("#flowers").html("");
+            $.each(data, function() { 
+                $.each(this, function(key, value) { 
+                    $("#flowers").append( 
+                        "<img src= "+ value.image + ">" +  
+                        "<br>" + value.name +  "<strong>" + "$" +
+                        value.price + "<strong>" + "<br>" + 
+                        "<button class=" + "btn btn-danger my-cart-btn" + "data-id=" + value.id + "data-name=" + value.name + 
+                        "data-summary=" + value.name +  "data-price=" + value.price + "data-quantity=" + "1" + "data-image=" + value.image
+                        + ">" + "Add to Cart"+ "</button>"
+                    ); 
+                }); 
+            });  
+        }
+    });
+});
