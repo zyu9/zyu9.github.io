e103d5ea58d8cb4f9cece4fdf69c8f128b5f5815
zyu9 <zyu9@uncc.edu>
2021-12-09 13:15:45 -0500

    Update featured.js

--- itis3135/project/scripts/featured.js
+++ itis3135/project/scripts/featured.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
             $.each(this, function(key, value) {  
                 $("#products").append( 
                     "<div class=" + "col-md-3 text-center" + ">" +
-                    "<img src= "+ value.image + ">" +  
+                    "<img src= "+ value.image + " alt=" + value.name + " width=300" + " height=200" + ">" +  
                     "<br>" + value.name + "<br>" + "</div>"
                 ); 
             }); 
