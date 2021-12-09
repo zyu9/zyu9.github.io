ada6999f752abddf95c32ccdd8a7f6204d5d6c67
zyu9 <zyu9@uncc.edu>
2021-12-09 13:08:43 -0500

    updated

--- itis3135/project/scripts/featured.js
+++ itis3135/project/scripts/featured.js
@@ -1,26 +1,14 @@
 $(document).ready(function() {
-    $.ajax({
-        type: "get",
-        url: "featured.json",
-        beforeSend: function() {
-            $("#products").html("Loading...");
-        },
-        timeout: 10000,
-        error: function(json, status, error) {
-            alert("Error: " + json.status + " - " + error);
-        },
-        dataType: "json",
-        success: function(data) {
-            $("#products").html("");
-            $.each(data, function() { 
-                $.each(this, function(key, value) { 
-                    $("#products").append( 
-                        "<div class=" + "col-md-3 text-center" + ">" +
-                        "<img src= "+ value.image + "alt=" + value.name + ">" +  
-                        "<br>" + value.name + "<br>" + "</div>"
-                    ); 
-                }); 
-            });  
-        }
-    });
+
+    $.getJSON("featured.json", function(data){ 
+        $.each(data, function() { 
+            $.each(this, function(key, value) {  
+                $("#products").append( 
+                    "<div class=" + "col-md-3 text-center" + ">" +
+                    "<img src= "+ value.image + "alt=" + value.name + ">" +  
+                    "<br>" + value.name + "<br>" + "</div>"
+                ); 
+            }); 
+        });  
+    }); 
 });
