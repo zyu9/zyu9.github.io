81e18f3aef90d5be95546387d2ab6a3fb0fd664c
zyu9 <zyu9@uncc.edu>
2021-12-09 16:54:45 -0500

    Update faqs.js

--- itis3135/project/scripts/faqs.js
+++ itis3135/project/scripts/faqs.js
@@ -3,11 +3,9 @@ $(document).ready(function() {
     var url = "faqs.json"; 
 
     $.getJSON(url, function(data) { 
-        var html = ""; 
         $.each(data.items, function(i, item){ 
-            html += "<h3>" + item.question + "</h3>" + "<div>"+ "<p>" + item.answer + "</p>" + "</div>";
+            $("#accordion").append("<h3>" + item.question + "</h3>" + "<div>"+ "<p>" + item.answer + "</p>" + "</div>");
         }); 
-        $("#accordion").html(html); 
 
         $("#accordion").accordion({
             event: "click",
