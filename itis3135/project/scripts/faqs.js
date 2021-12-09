6e6da5212bd1c1c22ee1aa3965498a72f0fbd2f7
zyu9 <zyu9@uncc.edu>
2021-12-09 17:07:01 -0500

    Update faqs.js

--- itis3135/project/scripts/faqs.js
+++ itis3135/project/scripts/faqs.js
@@ -6,11 +6,5 @@ $(document).ready(function() {
         $.each(data.items, function(i, item){ 
             $("#accordion").append("<h3>" + item.question + "</h3>" + "<div>"+ "<p>" + item.answer + "</p>" + "</div>");
         }); 
-
-        $("#accordion").accordion({
-            event: "click",
-            heightStyle: "content",
-            collapsible: true
-        });
     }); 
 });
